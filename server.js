a94cabe6b2e369ea819cad7b66931e5a50e36b2e
Glitch (vc-notification-bot) <none>
2021-08-09 15:45:59 +0000

    Added translation function!!!!!!

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const querystring = require("querystring");
-const path = require("path");
 const discord = require("discord.js");
 const client = new discord.Client();
 const config = require("./config.json");
 const prefix = config.prefix;
-const settings = require("./settings.json");
 const ytdl = require("ytdl-core");
-
 const Hypixel = require("hypixel-api-reborn");
 const hypixel = new Hypixel.Client(config.hypixelapi);
-const hypixelCommands = require("./commands/hypixelCommands.js")
+const hypixelCommands = require("./commands/hypixelCommands.js");
+const randomNumber = require("./commands/random number.js");
+const perfHooks = require("perf_hooks");
+const fetch = require("node-fetch");
 
 //npm install discord.js-commando
 //npm install discord.js
 //npm install ytdl-core
 //npm install hypixel-api-reborn
 
+
 client.once("ready", message => {
   console.log("Bot準備完了～");
   client.user.setPresence({ game: { name: "Bedwars 4v4v4v4" } });
 });
+
+
 
 if (config.token == undefined) {
   console.log("DISCORD_BOT_TOKENが設定されていません。");
@@ -28,6 +30,24 @@ if (config.token == undefined) {
 
 client.login(config.token);
 
+function sendDMMsg (userID , content) {
+  client.users.cache.get(userID).send(content)
+};
+
+function sendMsg (channelID , content) {
+  client.channels.cache.get(channelID).send(content)
+};
+
+
+client.on("presenceUpdate" , (oldPresence , newPresence) =>{
+  let yamato = oldPresence.user.id
+  let oldStatus = oldPresence.status;
+  let newStatus = newPresence.status;
+if(yamato != "738763915994857563") return;
+if(oldStatus != "online"){
+  sendDMMsg("739006634533060702" ,`大和さんが${oldStatus}から${newStatus}になりました！`)
+}
+});
 
 
 /*
@@ -39,7 +59,7 @@ client.on("message" , async msg =>{
     var response = collected.first()
     if(!response){
       msg.reply("タイムアウトしました。\n最初からやり直してください")}else{
-        var intNum = parseInt(response.content);８
+        var intNum = parseInt(response.content);
       if(Number.isFinite(intNum) && 0< intNum){
         if(response.content <101){
           var delNumber = intNum + 1;
@@ -54,33 +74,22 @@ client.on("message" , async msg =>{
     }
   }});
   */
-
-client.on("message", async msg => {
-  if (msg.content === "embed") {
-    msg.channel
-      .send({
-        embed: {
-          title: "メッセージ削除の確認",
-          fields: [
-            {
-              name: `あなたは個のメッセージを削除しようとしています。`,
-              value:
-                "本当に削除しますか？\n削除する場合は✓を、キャンセルする場合は×を選択してください。"
-            }
-          ],
-          color: 4303284
-        }
-      })
-      .then(msg2 => {
-        msg2.react("✅").then(msg2.react("❌"));
-        client.on("messageReactionAdd", (reaction, user) => {
-          if (user.id === msg.author.id) {
-            msg.channel.send(`${reaction.emoji.name} をリアクションしました`);
-          }
-        });
-      });
-  }
-});
+/*
+client.on("message" , async msg =>{
+  if(msg.content === "embed"){msg.channel.send({
+    embed: {
+      title: 'メッセージ削除の確認',
+      fields: [{ name: `あなたは個のメッセージを削除しようとしています。`, value: '本当に削除しますか？\n削除する場合は✓を、キャンセルする場合は×を選択してください。' }],
+      color: 4303284
+    }})
+  .then(msg2 =>{
+    msg2.react("✅")
+    .then(msg2.react("❌"))
+    client.on("messageReactionAdd" , (reaction, user) =>{
+      if(user.id === msg.author.id){
+        msg.channel.send(`${reaction.emoji.name} をリアクションしました`)
+      }})
+  })}});
 /*
 client.on("presenceUpdate" , (oldPresence , newPresence) =>{
  // let puser = oldPresence.user.id
@@ -94,17 +103,40 @@ if(oldStatus != "online" && newStatus === "online" ){
 }
 });
 */
-client.on("message", msg => {
-  if (!msg.content.startsWith(prefix + "mcid")) return;
-  let mcid = msg.content.split(" ");
-  let playerName = mcid[1];
-  hypixel.getPlayer(playerName).then(player => {
-    msg.channel.send(player.level);
-  });
+/*
+client.on("message" , msg =>{
+  if(!msg.content.startsWith(prefix + "mcid")) return;
+    let mcid = msg.content.split(" ");
+    let playerName = mcid[1];
+    hypixel.getPlayer(playerName)
+    .then(player =>{
+      msg.channel.send(player.level);
+    })
 });
 
+client.setInterval(() => {
+  let ch = client.channels.cache.get("786221980650700800");
+  hypixel.getWatchdogStats().then(wdr =>{
+    let wdrd = wdr.byWatchdogRollingDay;
+    let std = wdr.byStaffRollingDay;
+    let emb = new discord.MessageEmbed()
+    .setColor("#FF0000")
+                  .setTitle("Ban人数情報")
+                  .setDescription("24時間以内にBanされた人数を表示します")
+                  .addFields({name : "WatchdogによるBan" , value : `${wdrd}人` } , {name : "スタッフによるBan" , value : `${std}人`})
+                  .setFooter("24時間ごとに更新されます")
+                  .setTimestamp();
+  ch.send(emb);
+  })
+},  10*1000);
+
+*/
+
+
+
+/*
 function banInfo () {
-  let ch = client.channels.cache.get("858655976718204971");
+  let ch = client.channels.cache.get("786221980650700800");
 
   hypixel.getWatchdogStats().then(wdr => {
     let wdrd = wdr.byWatchdogRollingDay;
@@ -119,7 +151,7 @@ function banInfo () {
       )
       .setFooter("24時間ごとに更新されます")
       .setTimestamp();
-    ch.send(emb);
+      console.log(emb);
   });
 };
 
@@ -129,11 +161,11 @@ client.setTimeout(() =>
  {banInfo();
   client.setInterval(() => {
     banInfo();
-  } , 24 * 60 * 60 * 1000);
+  } , 1 * 1 * 20 * 1000);
 } , new Date().setHours(12, 0, 0, 0) - new Date());
 
 
-
+*/
 
 
 client.on("message" , msg => {
@@ -141,4 +173,67 @@ client.on("message" , msg => {
   if(msg.author.bot) return;
   if(!msg.content.startsWith(prefix + "hp")) return;
   hypixelCommands.call(msg);
+});
+
+
+
+client.on("message" , msg =>{
+  if(msg.author.bot) return;
+  if(msg.channel.type !== "dm") return;
+  sendDMMsg(config.ponDMID , `send by ${msg.author.username} \n${msg}`)
+  });
+
+  
+/*
+client.on("typingStart" , (channel , user) => {
+    if(!channel.id == "786221980650700800") return;
+    let userName = user.username;
+    sendMsg(channel.id , `${userName}様がタイピングしています。`)
+  });
+  */
+
+client.on ("message" , msg => {
+  if (!msg.content.startsWith(prefix + "rn")) return;
+  randomNumber.call(msg)
+});
+
+client.on("typingStart" ,async (channel , user) => {
+  if (channel.id !== "789853329147559986") return;
+  let startTime = perfHooks.performance.now();
+  console.log(startTime)
+  let filter = msg2 => msg2.author.id === user.id;
+  channel.awaitMessages(filter , {max : 1})
+  .then(() =>{
+    let endTime = perfHooks.performance.now();
+    console.log(endTime)
+    let mTime = endTime - startTime;
+    let Time = mTime / 1000;
+    channel.send(`あなたのタイピング時間は${Time}秒です。`)    }
+  )
+});
+
+let transMap = new Map();
+
+client.on("message", msg => {
+  if(msg.author.bot) return;
+  if(msg.content !== `${prefix}translate on`) return;
+  return transMap.set(msg.author.id, true);
+});
+
+client.on("message", msg => {
+  if(msg.author.bot) return;
+  if(msg.content !== `${prefix}translate off`) return;
+  return transMap.set(msg.author.id, false);
+});
+
+client.on("message", msg => {
+  if(!transMap.get(msg.author.id)) return;
+  if(msg.content == `${prefix}translate on`) return;
+  if(msg.content == `${prefix}translate off`) return;
+  if(msg.author.bot) return;
+  let content = msg.content;
+  let url = (`https://script.google.com/macros/s/AKfycbyeqscZvWix-RomnfPGiy1rCfMeVVd-vKtcdii_zpMWR2HF1Wf_IPt7o150gPyI1oEs/exec?text=${encodeURIComponent(content)}&source=ja&target=en`)
+  fetch(url).then(res => res.json()).then(data => {
+    msg.channel.send(data.text)
+  })
 });
